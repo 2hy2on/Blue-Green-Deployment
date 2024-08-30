feac403fc6911c520fba417b662052526f448ee9
mashin2002 <mashin0128@gmail.com>
2024-08-30 13:56:10 +0900

    init

---
+++ src/main/java/com/example/bluegreen/TestController.java
@@ -0,0 +1,14 @@
+package com.example.bluegreen;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> health(){
+        return ResponseEntity.ok("health");
+    }
+}
